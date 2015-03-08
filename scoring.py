b9b8f939590beec578fae554c65036d8d1e40bce
Michael Aldridge <aldridge.mac@gmail.com>
2015-03-08 12:13:35 -0500

    basic scoreboard library

---
+++ scoring.py
@@ -0,0 +1,57 @@
+import logging, json, os
+
+class ScoreBoard:
+    def __init__(self, dataDir):
+        self.logger = logging.getLogger(__name__)
+        self.dataDir = dataDir
+        try:
+            with open(os.path.join(dataDir, "scores.json"), 'r+') as f:
+                self.scores = json.load(f)
+        except:
+            with open(os.path.join(dataDir, "scores.json"), 'w+') as f:
+                self.scores = {}
+
+    def sync(self):
+        self.logger.debug("Syncing database")
+        with open(os.path.join(self.dataDir, "scores.json"), 'w') as f:
+            json.dump(self.scores, f)
+
+    def solve(self, team, problem, points):
+        if not team in self.scores:
+            self.scores[team] = {}
+        if not problem in self.scores[team]:
+            self.scores[team][problem] = False
+        if not "score" in self.scores[team]:
+            self.scores[team]["score"] = 0
+        if self.scores[team][problem] == False:
+            self.scores[team][problem] = True
+            self.scores[team]["score"] += points
+            self.sync()
+            return True
+        else:
+            self.logger.info("Team %s attempted duplicate solve", team)
+            return False
+
+    def hasSolved(self, team, problem):
+        try:
+            return self.scores[team][problem]
+        except KeyError:
+            return False
+
+    def getScore(self, team):
+        return self.scores[team]["score"]
+
+    def getRanks(self):
+        rankedTeams = []
+        for team in self.scores.keys():
+            rankedTeams.append((team, self.scores[team]["score"]))
+        return sorted(rankedTeams, key = lambda team: team[1])
+
+if __name__ == "__main__":
+    s = ScoreBoard("data")
+    s.solve("team1", "problem1", 100)
+    s.solve("team2", "problem1", 130)
+    s.solve("team3", "problem1", 13)
+    s.solve("team4", "problem1", 153)
+    s.solve("team5", "problem1", 170)
+    print s.getRanks()
