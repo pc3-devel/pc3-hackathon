43c9525931ef0a99e2eb4987ddb2673c7834c92c
Ian Joseph Brown <ianjosephbrown@gmail.com>
2015-03-08 07:39:06 -0500

    Added comments for readability

--- loader.py
+++ loader.py
@@ -1,16 +1,20 @@
 import logging, glob, os, json, sys, collections
 
 class Loader:
-    
     def __init__(self, directoryPath):
-        self.logger = logging.getLogger("loader")
-        problemsExists = False
         self.problemsDictionary = {}
+
+        #allows for logging of messages during execution
+        self.logger = logging.getLogger("loader")
+
+        #verifies that root directory contains problems directory
+        problemsExists = False
         for dirs in os.listdir(directoryPath):
             if (dirs == "problems"):
                 problemsExists = True
         
         if (problemsExists == True):
+            #builds initializing dictionary
             self.problemsDictionary = self.createProblemsDict(os.path.join(directoryPath, "problems"))
 
         else:
@@ -19,19 +23,21 @@ class Loader:
 
     def createProblemsDict(self, path):
         tmpProblemsDictionary = {}
-        defaultPath = path
         self.meta_defaults = {}
+        defaultPath = path
+
         self.meta_defaults = self.metaDefaultsIn(os.path.join(defaultPath, "meta_defaults.json"))
         
+        #finds subdirectories of problems dir and makes those the 
+        #dictionary keys through passing them to the probStats function
         for problem in os.listdir(path):
             if (os.path.isdir(os.path.join(path,problem)) == True):
-                print tmpProblemsDictionary
-                print '\n'
                 tmpProblemsDictionary.update(self.probStats(path, problem, self.meta_defaults))
 
         return tmpProblemsDictionary
 
-
+    #Establishes meta default values by reading in from the meta-defaults.json
+    #file
     def metaDefaultsIn(self, defaultPath):
         defaults = {}
         
@@ -40,16 +46,21 @@ class Loader:
 
         return defaults
 
+    #Defines each problem and its attributes within the dictionary
     def probStats(self, path, problem, meta_defaults):
-        problemPath = os.path.join(path, problem)
         problemList = []
         tmpDict = {}
+        problemPath = os.path.join(path, problem)
 
         self.logger.info("Logging " + problem);
         
+        #Puts description in the problemList
         with open(os.path.join(problemPath, "description.txt")) as desc:
             problemList.append(desc.read())
 
+        #Decides whether to use each meta value within the problem directory or
+        #the default value if there is no value (null) for the dictionary value
+        #and adds it to the problemList
         with open(os.path.join(problemPath, "meta.json")) as meta:
             tmpDict = json.load(meta)
 
@@ -59,16 +70,18 @@ class Loader:
 
         problemList.append(tmpDict)
 
-
+        #Checks whether the input values exist and, if so, it adds it to the
+        #problemList
         try:
             problemList.append(os.path.join(problemPath, "in.txt"))
 
         except:
             self.logger.error("Unexpected Error: " + sys.exc_info()[0])
 
+        #Add out.txt to problemList
         problemList.append(os.path.join(problemPath, "out.txt"))
 
-
+        #Return problem to be stored in the dictionary
         return {problem: problemList}
 
 
