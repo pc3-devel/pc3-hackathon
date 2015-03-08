df94479fa612a0c9d551722f669985224009d674
Michael Aldridge <aldridge.mac@gmail.com>
2015-03-08 05:03:08 -0500

    create util.py to handle run manipulation

--- util.py
+++ util.py
@@ -1,20 +1,30 @@
-import os, glob
+import os, glob, CodeRunner, logging
 
-def getTeamRunNum(teamPath, problem):
-    path = os.path.join(teamPath, problem, '*')
-    runs = glob.glob(path)
-    return len(runs)+1
+class Util:
+    def __init__(self, dataDir):
+        self.logger = logging.getLogger("util")
+        self.logger.debug("Data dir is: " + dataDir)
+        self.evaluator = CodeRunner.Evaluate(dataDir)
 
-def getTeamDataPath(dataDir, team):
-    return os.path.join(dataDir, "teamData", team)
+    def getTeamRunNum(self, teamPath, problem):
+        path = os.path.join(teamPath, problem, '*')
+        runs = glob.glob(path)
+        return len(runs)+1
 
-def makeRun(dataDir, team, problem):
-    teamPath = getTeamDataPath(dataDir, team)
-    runNum = getTeamRunNum(teamPath, problem)
-    runPath = os.path.join(teamPath, problem, str(runNum))
-    os.makedirs(runPath)
-    return runPath
+    def getTeamDataPath(self, dataDir, team):
+        return os.path.join(dataDir, "teamData", team)
+
+    def makeRun(self, dataDir, team, problem):
+        teamPath = self.getTeamDataPath(dataDir, team)
+        runNum = self.getTeamRunNum(teamPath, problem)
+        runPath = os.path.join(teamPath, problem, str(runNum))
+        os.makedirs(runPath)
+        return runPath
+
+    def doRun(self, team, problem):
+        pass
 
 if __name__=="__main__":
-    print getTeamRunNum(getTeamDataPath("data", "team1"), "problem1")
-    makeRun("data", "team1", "problem1", None)
+    u = Util("data")
+    print u.getTeamRunNum(getTeamDataPath("data", "team1"), "problem1")
+    u.makeRun("data", "team1", "problem1", None)
