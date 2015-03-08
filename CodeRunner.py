dc8ae14357fb3868c643c2c4f5f9ecdf56b336c6
Michael Aldridge <aldridge.mac@gmail.com>
2015-03-08 05:04:10 -0500

    add compare function with placeholder good value

--- CodeRunner.py
+++ CodeRunner.py
@@ -3,6 +3,7 @@ import logging, languageSupport, subprocess
 class Evaluate:
     def __init__(self, dataDir):
         self.logger = logging.getLogger("Evaluator")
+        self.logger.info("Loading an evaluator")
         self.support = languageSupport.LangSupport(dataDir)
 
     def evaluate(self, lang, teamFile):
@@ -14,7 +15,10 @@ class Evaluate:
     def _run(self, runTime, baseName):
         cmd = runTime.replace("{base}", baseName).split()
         output = subprocess.Popen(cmd, stdout=subprocess.PIPE)
-        return output
+        return compare(output.stdout.read(), "Hello World!")
+
+    def compare(self, run, good):
+        return run == good
 
 if __name__=="__main__":
     logging.basicConfig(level=logging.DEBUG)
